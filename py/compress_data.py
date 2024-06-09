423a7f9be3e3769fbbd0a6b717874b9d6c7bc072
dcmvdbekerom <dcmvdbekerom@gmail.com>
2024-06-09 11:11:26 +0200

    Compress data

---
+++ py/compress_data.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  9 11:00:16 2024
+
+@author: bekeromdcmvd
+"""
+
+import numpy as np
+
+fnames = [
+    'nu_data.npy',
+    'sigma_gRmin_data.npy',
+    'E0_data.npy',
+    'J_clip_data.npy',
+    ]
+
+fpath = '../data/'
+
+for fname in fnames:
+    print('Compressing '+fname+'... ', end='')
+    arr = np.load(fpath + fname)
+    np.savez_compressed(fpath+fname[:-1]+'z', arr)
+    print('Done!')
